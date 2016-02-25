2d6bc990dc726d1dbf6556967fc81321d39d3ef7
naive-liu <lxy_pku@qq.com>
2016-02-25 22:40:13 +0800

    commit

--- src/com/example/app/Point.java
+++ src/com/example/app/Point.java
@@ -17,19 +17,19 @@ public class Point {
 	}
 	//get distance (m)
 	public double getDistance(Point p) {  
-	    double a,b,R;
-	    R = 6378137;
-	    a = rad(lat) - rad(p.lat);
-	    b = rad(lon) - rad(p.lon);
-	    double d;  
-	    double sa2, sb2;  
-	    sa2 = Math.sin(a / 2.0);  
-	    sb2 = Math.sin(b / 2.0);  
-	    d = 2  
-	            * R  
-	            * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(rad(lat))  
-	                    * Math.cos(rad(p.lat)) * sb2 * sb2));
-	    return d;  
+		double a,b,R;
+		R = 6378137;
+		a = rad(lat) - rad(p.lat);
+		b = rad(lon) - rad(p.lon);
+		double d;
+		double sa2, sb2;
+		sa2 = Math.sin(a / 2.0);
+		sb2 = Math.sin(b / 2.0);
+		d = 2  
+				* R  
+				* Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(rad(lat))  
+						* Math.cos(rad(p.lat)) * sb2 * sb2));
+		return d;
 	}
 	
 	public Point random(double deviation)
--- src/com/example/app/Track.java
+++ src/com/example/app/Track.java
@@ -5,9 +5,10 @@ public class Track {
 	private int pointnum;
 	private Point curpoint;
 	private int pre;//index of previous key point
-	private double interval=1500.0;
-	private double EPSILON=0.00000001;
+	private double interval=1500.0;//time interval between two points (ms)
+	private boolean random_track_flag=true;//Whether to set a random deviation on the track
 	private double deviation=1.0/200000.0;
+	private double EPSILON=0.00000001;
 	Track(double [] lats_, double [] lons_, int pointnum_)
 	{
 		pointnum=pointnum_;
@@ -28,10 +29,10 @@ public class Track {
 		{
 			int time=(int)(dis_keypoint/(speed /3.6) * 1000);
 			try {
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+				Thread.sleep(time);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			pre=next;
 			curpoint=keypoint[pre];
 		}
@@ -41,13 +42,13 @@ public class Track {
 			double londiff= keypoint[next].lon -curpoint.lon;
 			double rate=dis_interval/dis_keypoint;
 			try {
-                Thread.sleep((int)interval);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+				Thread.sleep((int)interval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			curpoint=new Point(curpoint.lat+latdiff*rate,curpoint.lon+londiff*rate);;
 		}
-		curpoint=curpoint.random(deviation);
+		if(random_track_flag)curpoint=curpoint.random(deviation);
 		return curpoint;
 	}
 }
